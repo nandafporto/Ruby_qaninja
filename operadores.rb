9453d1fc259188acbec02be657635937c64211b2
Amanda Porto <porto.ferreira.1337@gmail.com>
2019-03-11 08:02:40 -0300

    Operadores em Ruby

---
+++ operadores.rb
@@ -0,0 +1,34 @@
+
+numero1 = 0
+numero2 = 0
+total = 0
+
+=begin  puts 'Digita o primeiro valor'
+numero1 = gets.chomp.to_i
+
+puts 'Digite o segundo valor'
+numero2 = gets.chomp.to_i
+
+total = numero1 + numero2
+puts total 
+=end
+
+#Comentarios em bloco, no começo da linha =begin, no final =end (ter q ser no começo da linha)
+
+#Operadores de Comparação
+
+n1 = 10
+n2 = 11
+puts n1 > n2 #maior
+puts n1 != n2 #diferente
+puts n1 >= n2 #maior igual
+puts n1 == n2 #igual
+puts n1.eql?(n2) #igual
+
+
+
+
+
+
+
+
