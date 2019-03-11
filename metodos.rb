3039c2220ee89128730f81af59b4a295976cf69e
Amanda Porto <porto.ferreira.1337@gmail.com>
2019-03-11 08:01:08 -0300

    Métodos em Ruby

---
+++ metodos.rb
@@ -0,0 +1,26 @@
+
+#Estrutura para definir um metodo
+def diga_ola(nome)
+    puts'Olá! ' + nome
+end
+
+#invocar o metodo
+diga_ola('Amanda')
+
+# def retorna_nome
+#     'Amanda'
+# end
+
+# resultado = retorna_nome
+# puts resultado
+
+# def soma(v1,v2)
+#     total = v1 + v2
+#     total
+# end
+
+# res = soma(10,5)
+# puts res
+
+#para comentar varias linhas, seleciona as linhas ctrl + ;
+
