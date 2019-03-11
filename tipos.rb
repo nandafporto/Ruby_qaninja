c3ece8306da76bd32a4609061d5f66b476eeefec
Amanda Porto <porto.ferreira.1337@gmail.com>
2019-03-11 07:55:08 -0300

    Tipos de variáveis no Ruby

---
+++ tipos.rb
@@ -0,0 +1,23 @@
+
+
+# Tipos de variáveis em ruby, é do tipo dinamico, é definido no momento que ela recebe o valor
+
+produto = 'Notebook'
+
+puts produto.class 
+
+#para saber o tipo da variavel, é só chamar a classe usando
+# o puts variavel.class
+
+preco = 2999
+disponivel = true
+taxa = 1.99
+
+puts preco.class
+puts disponivel.class
+puts taxa.class
+
+puts produto
+puts preco
+puts disponivel
+puts taxa
